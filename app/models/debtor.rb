5ec9203abdc9a1008af6be8532df6329b05a6916
Denver Stove <dtstove@gmail.com>
2018-09-08 11:59:16 +1000

    Score out of 1000 is now presented

--- app/models/debtor.rb
+++ app/models/debtor.rb
@@ -1,2 +1,13 @@
 class Debtor < ApplicationRecord
+  after_create :calculate_score
+end
+
+def calculate_score
+  @score = 0
+  if self.sex.capitalize == "Male"
+    @score += 700
+  else self.sex.capitalize == "Female"
+    @score += 300
+  end
+  self.update(score: @score)
 end
