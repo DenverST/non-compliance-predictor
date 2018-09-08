2f06d6d74b660caed46555cff05499b519306297
Denver Stove <dtstove@gmail.com>
2018-09-08 10:13:16 +1000

    Working on building a few pages, like the simple form

--- app/controllers/debtors_controller.rb
+++ app/controllers/debtors_controller.rb
@@ -1,2 +1,31 @@
 class DebtorsController < ApplicationController
+
+def index
+  @debtors = Debtor.all
+end
+
+def show
+  @debtor = Debtor.find(params[:id])
+end
+
+def new
+  @debtor = Debtor.new
+end
+
+def create
+  @debtor = Debtor.new(debtor_params)
+  if @debtor.save!
+    redirect_to
+  else
+    render :new
+  end
+end
+
+
+private
+
+  def debtor_params
+    params.require(:debtor).permit(:unique_id, :year, :area, :sex, :family, :occupation, :cause, :income, :unsecured_debts, :value_of_assets, :non_compliance_type)
+  end
+
 end
