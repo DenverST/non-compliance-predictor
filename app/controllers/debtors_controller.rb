3d3593e9729d85121147f8cccc28edc4cb12981c
Denver Stove <dtstove@gmail.com>
2018-09-08 12:51:09 +1000

    Implemented Score and a currently at risk page, help page also completed

--- app/controllers/debtors_controller.rb
+++ app/controllers/debtors_controller.rb
@@ -8,6 +8,17 @@ def show
   @debtor = Debtor.find(params[:id])
 end
 
+def current
+  # @current_debtors = Debtor.where("?".to_i > 600, :score)
+  @current_debtors = []
+  Debtor.all.each do |debtor|
+    if (debtor.score > 600) && (debtor.year.to_i > 2016)
+      @current_debtors << debtor
+    end
+  end
+  @current_debtors
+end
+
 def new
   @debtor = Debtor.new
 end
