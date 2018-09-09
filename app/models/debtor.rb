5a80492938cd1c536c1b8ee6e4237c8c46eb03f7
Denver Stove <dtstove@gmail.com>
2018-09-09 12:04:40 +1000

    Score does not work properly, there is something fake in its place

--- app/models/debtor.rb
+++ app/models/debtor.rb
@@ -2,86 +2,119 @@ class Debtor < ApplicationRecord
   after_create :calculate_score
 end
 
-BIGHASH = {
+SEX = {
   "Female" => 0,
   "Male" => 1,
+  "Not Stated" => 2,
+  "Unknown" => 3,
+  nil => 0
+}
+
+FAMILY = {
+  "Single without Dependants" => 0,
   "Single with Dependants" => 1,
   "Couple without Dependants" => 2,
   "Couple with Dependants" => 3,
-  "Single without Dependants" => 4,
-  "Unemployment or loss of income" => 21,
-  "Ill health or absence of health insurance" => 10,
-  "Other business reason or reason unknown" => 16,
-  "Domestic discord or relationship breakdowns" => 2,
-  "Excessive use of credit facilities including losses on repossessions, high interest payments and pressure selling" => 6,
-  '"Personal reasons' => 19,
-  "Other causes or causes unknown" => 17,
-  '"Inability to collect debts due to disputes, faulty work or bad debts"' => 11,
-  "Failure to keep proper books of account and costing record" => 7,
-  '"Economic conditions affecting industry, including competition, credit restrictions, fall in prices or increases in cost"' => 3,
-  '"Gambling, speculation & extravagance in living"' => 9,
+  "Unknown" => 4,
+  "Not Stated" => 5,
+  nil => 0
+}
+
+CAUSE = {
   "Adverse legal action" => 0,
-  "Excessive drawings including failure to provide for taxation" => 4,
-  "Excessive interest payments on loan monies and capital losses on repayments" => 5,
+  "Domestic discord or relationship breakdown" => 1,
+  "Domestic discord or relationship breakdowns" => 2,
+  '"Economic conditions affecting industry, including competition, credit restrictions, fall in prices or increases in cost"' => 3,
+  'Excessive drawings including failure to provide for taxation' => 4,
+  'Excessive interest payments on loan monies and capital losses on repayments' => 5,
+  "Excessive use of credit facilities including losses on repossessions, high interest payments and pressure selling" => 6,
+  "Failure to keep proper books of account and costing record" => 7,
   "Gambling or speculation" => 8,
+  '"Gambling, speculation & extravagance in living"' => 9,
+  "Ill health or absence of health insurance" => 10,
+  '"Inability to collect debts due to disputes, faulty work or bad debts"' => 11,
   "Lack of business ability including underquoting or failure to assess potential of business" => 12,
   "Lack of sufficient initial working capital" => 13,
   "Liabilities due to guarantees" => 14,
   "Not Stated" => 15,
+  "Other business reason or reason unknown" => 16,
+  "Other causes or causes unknown" => 17,
+  "Personal reasons, including ill health of self or dependents, domestic discord & other personal reasons" => 18,
+  '"Personal reasons' => 19,
   "Seasonal conditions including floods and drought" => 20,
+  "Unemployment or loss of income" => 21,
   "Unknown" => 22,
-  "Yes" => 1,
+  nil => 0
+ }
+
+ INCOME = {
+  '$0-$49999' => 0,
+  '$50000-$99999' => 1,
+  '$100000-$149999' => 2,
+  '$150000-$199999' => 3,
+  'More Than $200000' => 4,
+  nil => 0
+ }
+
+ BUSINESS = {
   # Business Related
   "No" => 0,
-  "$0-$49999" => 0,
-  "$50000-$99999" => 1,
-  "$100000-$149999" => 2,
-  "$150000-$199999" => 3,
-  "Government benefits/Pensions" => 2,
-  "Gross Wages and Salary" => 3,
-  "Unknown" => 10,
-  "Self Employment" => 8,
-  "Business earnings" => 0
+  "Yes" => 1,
+  "Not Stated or Unknown" => 2,
+  nil => 0
 }
 
-BIGHASHTWO {
+PRIMARY = {
+  'Business earnings' => 0,
+  'Deceased Estate or Trusts' => 1,
+  'Government benefits/Pensions' => 10,
+  'Gross Wages and Salary' => 2,
+  'Income from Investments' => 3,
+  'Income from reverse mortgage' => 4,
+  'Lump Sum termination payments' => 5,
+  'Other' => 6,
+  'Self Employment' => 7,
+  'Superannuation' => 8,
+  'Unknown' => 9,
+   nil => 0
+}
+
+DEBTS = {
   # unsec debts
   "$0-$49999" => 0,
-  "$50000-$99999" => 9,
-  "$500000-$549999" => 10,
-  "$600000-$649999" => 12,
-  "$100000-$149999" => 1,
-  "$200000-$249999" => 3,
-  "$450000-$499999" => 8,
-  "$200000-$249999" => 6,
-  "$150000-$199999" => 2,
-  "$300000-$349999" => 5,
-  "$350000-$399999" => 11,
-  "$750000-$799999" => 15,
-  "$250000-$299999" => 4,
-  "$400000-$449999" => 7,
-  "$700000-$749999" => 14,
-  "$650000-$699999" => 13,
-  "$900000-$949999" => 18,
-  "$850000-$899999" => 17,
-  "$900000-$900000"=> 16,
-  "$950000-$999999" => 19,
-  "More Than $1000000" => 20,
-  "Registered Trustee" => 1,
+  "$$250000-$299999" => 1,
+  "$450000-$499999" => 2,
+  "$300000-$349999" => 3,
+  "$550000-$599999" => 4,
+  "$500000-$549999" => 5,
+  "$400000-$449999" => 6,
+  "$600000-$649999" => 7,
+  "$350000-$399999" => 8,
+  "$100000-$149999" => 9,
+  "$150000-$199999" => 10,
+  "'$950000-$999999" => 11,
+  "$200000-$249999" => 12,
+  "$700000-$749999" => 13,
+  "$$650000-$699999" => 14,
+  "$50000-$99999" => 15,
+  "More Than $1000000" => 16,
+  "$800000-$849999" => 17,
+  "$750000-$799999" => 18,
+  "$900000-$949999" => 19,
+  "$850000-$899999" => 20,
+  nil => 0
 }
 
+# def calculate_score
+#   @score = 1 + Math.exp(-(-4.69812858 + (3.98211350e-01 * (SEX[:sex] || 0).to_f + (1.16155944e-01 * (FAMILY[:family] || 0)).to_f + (-7.15849006e-03 * (self.occupation  || 0).to_i).to_f + (1.51790148e-02 * (CAUSE[:cause] || 0)).to_f + (1.00351347e+00 * (BUSINESS[:business] || 0)).to_f + (4.27699874e-02 * (INCOME[:income] || 0)).to_f + (-3.09722201e-02 * (PRIMARY[:income_source] || 0)).to_f + (7.24779573e-02 * (DEBTS[:unsecured_debts] || 0)).to_f + (2.44275831e-02 * (DEBTS[:value_of_assets] || 0)).to_f + (9.80159282e+00 * (self.num_instances || 0).to_i).to_f))).to_f
+#   @score = (1/@score).to_f
+#   self.update(score: @score)
+#   Giving really low numbers
+# end
+
 def calculate_score
-  @score = 1 + exp(-(-4.04117189 + (-8.52492653e-02 * BIGHASH[:area]) + (-1.53843695e-03 * BIGHASH[:sex]) + (4.01603385e-01 * BIGHASH[:family]) + (1.17323380e-01 * (self.occupation).to_i) + (-6.19945090e-03 * BIGHASH[:cause]) + (1.70202585e-02 * BIGHASH[:business]) + (1.06435564e+00 * BIGHASH[:income]) +  (1.04708405e-01 * BIGHASH[:income_source]) +  (-4.85736631e-02 * BIGHASHTWO[:unsecured_debts]) + (6.84105205e-02 * BIGHASHTWO[:value_of_assets]) + (2.98097017e-02 * (self.non_compliance_type ? 1 : 0)) + (1.00908709e+01 * (self.num_instances).to_i)))
-  @score = 1/@score
+  @score = rand(150..900)
   self.update(score: @score)
 end
 
-coefficients = [-8.52492653e-02, -1.53843695e-03,  4.01603385e-01,
-         1.17323380e-01, -6.19945090e-03,  1.70202585e-02,
-         1.06435564e+00,  1.04708405e-01, -4.85736631e-02,
-         6.84105205e-02,  2.98097017e-02,  1.00908709e+01]
-intercept = -4.04117189
-p = 1/(1 + exp(-(-4.04117189 + -8.52492653e-02*X1 + (-1.53843695e-03)*X2 + 4.01603385e-01 * X3 + 1.17323380e-01 * X4 + -6.19945090e-03 * X5 + 1.70202585e-02 * X6 + 1.06435564e+00 * X7 +  1.04708405e-01 * X8 +  -4.85736631e-02 * X9 + 6.84105205e-02 * X10 + 2.98097017e-02 * X11 + 1.00908709e+01 * X12)))
 
- 428: 3116955,2010,Belconnen,80101,Australian Capital Territory,8ACTE,Australian Capital Territory,Male,Unknown,,,Unknown,Not Stated or Unknown,$0-$49999,Unknown,$0-$49999,$0-$49999,Administration,Offence Referral,None,0,$0,
- 472: 3001355,2014,Gungahlin,80104,Australian Capital Territory,8ACTE,Australian Capital Territory,Male,Couple with Dependants,82,Construction and Mining Labourers,Other causes or causes unknown,No,$50000-$99999,Business earnings,$100000-$149999,$0-$49999,Bankrupt,Objection to Discharge,Extension to Bankruptcy Period,1,8 Years
