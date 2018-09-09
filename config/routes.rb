d4e1bcc6c4c6afa108802e865452d08bed0088ac
Jody Vanden <jody.vanden@gmail.com>
2018-09-09 15:54:30 +1000

    page compliance_risk_map_3 done

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root to: 'pages#home', as: :home
   get '/compliance_risk_app_1', to: 'pages#compliance_risk_maps_1'
   get '/compliance_risk_app_2', to: 'pages#compliance_risk_maps_2'
+  get '/compliance_risk_app_3', to: 'pages#compliance_risk_maps_3'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
