e1986e99b9d6b6d68c026cb284a94282463691a4
Aaron Mendonsa <aaron.mendonsa@gmail.com>
2018-09-09 14:02:56 +1000

    Changed navbar,etc

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # root to: 'pages#home'
   resources :debtors
   get '/help', to: 'pages#help', as: :help
-  root to: 'debtors#current', as: :current
+  root to: 'pages#home', as: :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
